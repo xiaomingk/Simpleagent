9e875589f1f3fddbaccbd402fea622d2d892ad52
xiaomingk <kanx@chalmers.se>
2021-01-19 21:27:13 +0100

    Updated Code

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='Agent',
-    version='0.0.5',
+    version='0.0.6',
     description='My agent based model package',
-    url='https://github.com/xiaomingk/Simpleagent',
+    url='https://github.com/xiaomingk/Simpleagent.it',
     author='Xiaoming Kan',
     author_email='kanx@chalmers.se',
     license='unlicense',
